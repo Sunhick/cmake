d7df14c354044a1229711f5552b9f8b35dbce4a2
Sunil <sunhick@gmail.com>
2017-03-23 22:08:53 -0600

    removed debug files

--- rbtrees/Xcode/rbtree.build/Debug/ALL_BUILD.build/Script-030D980A23FD40AEB77E5ACB.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Sunny/prv/github/cmake/rbtrees/Xcode -f /Users/Sunny/prv/github/cmake/rbtrees/Xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- rbtrees/Xcode/rbtree.build/Debug/ZERO_CHECK.build/Script-4AFDE4B3AAB345FF9E013D78.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Sunny/prv/github/cmake/rbtrees/Xcode -f /Users/Sunny/prv/github/cmake/rbtrees/Xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
