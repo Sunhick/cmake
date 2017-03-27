58511f5d95114dfbffe7626ddcad5801ba29b863
Sunil <sunhick@gmail.com>
2017-03-27 02:20:43 -0600

    implemented rb tree add node

--- rbt/main.cpp
+++ rbt/main.cpp
@@ -16,12 +16,17 @@
 
 using namespace std;
 
+void die(string message) {
+    cout << message << endl;
+    exit(0);
+}
+
 void PopulateCatalog(MovieTree* catalog, string filename) {
     ifstream file(filename);
     
     // non-existant or corrupted file
     if(file.fail()) {
-        throw runtime_error("Check if file exists!");
+        die("Could not open file");
     }
     
     int ranking, releaseYear, quantity;
@@ -52,15 +57,16 @@ void PopulateCatalog(MovieTree* catalog, string filename) {
 int main(int argc, const char * argv[]) {
     
     string dmenu =  "======Main Menu======\n"
-    "1. Find a movie\n"
-    "2. Rent a movie\n"
-    "3. Print the inventory\n"
-    "4. Delete a movie\n"
-    "5. Count the movies\n"
-    "6. Quit\n";
+                    "1. Rent a movie\n"
+                    "2. Print the inventory\n"
+                    "3. Delete a movie\n"
+                    "4. Count the movies\n"
+                    "5. Count the longest path\n"
+                    "6. Quit\n";
+    
     
     if (argc < 2){
-        throw runtime_error("Missing movie collection file!");
+        die("Missing movie collection file!");
     }
     
     int choice = 0;
@@ -90,20 +96,11 @@ int main(int argc, const char * argv[]) {
                 
             case 2:
             {
-                string title;
-                cout << "Enter title:" << endl;
-                std::getline(cin, title);
-                catalog.rentMovie(title);
-                break;
-            }
-                
-            case 3:
-            {
                 catalog.printMovieInventory();
                 break;
             }
                 
-            case 4:
+            case 3:
             {
                 string title;
                 cout << "Enter title:" << endl;
@@ -112,17 +109,28 @@ int main(int argc, const char * argv[]) {
                 break;
             }
                 
-            case 5:
+            case 4:
             {
                 cout << "Tree contains: " << catalog.countMovieNodes() << " movies." << endl;
                 break;
             }
                 
+            case 5:
+            {
+                cout << "Longest Path: " << catalog.countLongestPath() << endl;
+                break;
+            }
+                
             case 6:
-            default:
             {
                 done = true;
                 break;
+            }
+                
+            default:
+            {
+                // ignore unrecognized input.
+                // and let program continue.
             }
         }
         
